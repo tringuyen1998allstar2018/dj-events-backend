5fbbc43fd7bfb2252a2ea69584b6959722305681
Nguyen Minh Tri <tringuyen1998allstar2018@gmail.com>
2021-07-18 04:41:44 +0700

    Init Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd9722924f6ce7ac052fd953d4a485706'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '12774ece-f2ec-4dff-86a6-7441220ee9d8'
+};
